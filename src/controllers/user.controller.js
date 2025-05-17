f3f05a9b625e1f208631ab620e109ca7cfc42ec5
iamapoorv476 <apoorvapratapsingh6@gmail.com>
2025-05-17 15:19:36 +0530

    Added user controller

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,40 @@
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken"
+import mongoose from "mongoose";
+import { use } from "react";
+import { User } from "../models/user.model.js";
+
+const usersignup = asyncHandler(async(requestAnimationFrame,res)=>{
+    const {fullName,username,email,password} = req.body
+
+    if(
+        [fullName,username,email,password].some((field)=> field ?.trim()==="")
+    ){
+        throw new ApiError(400,"All feilds are required")
+    }
+    const existedUser = await User.findOne({
+        $or:[{username},{email}]
+    })
+    if(existedUser){
+        throw new ApiError(409,"User with email or username already exist!")
+    }
+    const user = await User.create({
+        fullName,
+        username,
+        email,
+        password
+    })
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+    if (!createdUser) {
+        throw new ApiError(500, "Something went wrong while registering the user")
+    }
+
+    
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User registered Successfully")
+    )
+})
